d46b13e04cf8881ba085f122c7e3eaa7db0b9b75
amirSeify <amir1376.seify@gmail.com>
2022-10-09 14:39:29 +0330

    feat: init

---
+++ src/App.tsx
@@ -0,0 +1,73 @@
+import { useState } from 'react'
+import './App.css'
+
+import { AudioRecorder, IDataAvailable } from "@amirseifi/react-voice-recorder"
+import "@amirseifi/react-voice-recorder/dist/style.css"
+
+function App() {
+
+  const [isRecordMode, setIsRecordMode] = useState(false)
+  const [audioSrc, setAudioSrc] = useState("")
+
+
+  const onDataReady = (value: IDataAvailable) => {
+    console.log(value);
+
+    const reader = new FileReader()
+
+    reader.onload = (event) => {
+      if (event.target !== null)
+        setAudioSrc(event.target.result as string)
+    }
+    reader.readAsDataURL(value.value)
+
+    setIsRecordMode(false)
+  }
+  const onCancel = () => {
+    setIsRecordMode(false)
+    setAudioSrc("")
+  }
+
+  const onPermissionDenied = () => {
+    setIsRecordMode(false)
+    setAudioSrc("")
+    alert("Permission Denied")
+  }
+
+  return (
+    <div className="App">
+
+      {
+        audioSrc && (
+          <audio src={audioSrc} controls />
+        )
+      }
+
+      <div className='chat-control'>
+        <div className='chat-control-container'>
+
+
+          {
+            !isRecordMode && (
+              <button onClick={() => { setIsRecordMode(!isRecordMode) }}>click to record</button>
+            )
+          }
+          <div className='voice-recorder'>
+            {
+              isRecordMode && (
+                <AudioRecorder
+                  onCancel={onCancel}
+                  onDataAvailable={onDataReady}
+                  onPermissionDenied={onPermissionDenied}
+                  isLogging
+                />
+              )
+            }
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
